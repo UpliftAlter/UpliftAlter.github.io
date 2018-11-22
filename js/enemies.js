849b9802fc36861aa7dfcd5ac0b4440e2da6466e
UpliftAlter <34913266+UpliftAlter@users.noreply.github.com>
2018-11-22 16:39:26 +0200

    V1.0 BOMBERMANGO
    YEAH

---
+++ js/enemies.js
@@ -0,0 +1,81 @@
+var enemies = [];
+var enemy;
+
+//CREATING ENEMIES
+function spawnEnemies(){
+	
+	//NUMBER OF ENEMIES GENERATED RANDOMLY
+	var random = Math.floor((Math.random() * 10) + 1);
+	
+	//LOADING THE PICTURES
+	var iconennemies = 'Images/enemy.png';
+	
+	//CREATING AND LOCATING ENEMIES
+	for (var i = 0; i < random; i++) {	
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function(position) {	
+				
+				//RANDOMLY LOCATING
+				var randomLat = (Math.random() * (- 0.0050 - 0.0050) + 0.0050);
+				var randomLng = (Math.random() * (- 0.0050 - 0.0050) + 0.0050);
+			
+				var enemyPos ={
+					lat: position.coords.latitude + randomLat,
+					lng: position.coords.longitude + randomLng
+				};
+				
+				var markerEnemy = new google.maps.Marker({
+					draggable: false,
+					position: enemyPos,
+					map: map,
+					icon : iconennemies
+				});
+				
+				//CREATING AND STORING ENEMY OBJECTS IN TO AN ARRAY
+				enemy = ({markerE: markerEnemy,lat : enemyPos.lat , lng:enemyPos.lng , life : 1});
+				enemies.push(enemy);	
+			});
+        }
+	}
+	enemyAction();
+	enemyBomb();
+}
+
+function enemyAction(){
+	for(var i=0; i<enemies.length; i++){
+		
+		var randomVelLat = (Math.random() * (- 0.0002870 - 0.0002870) + 0.0002870);
+		var randomVelLng = (Math.random() * (- 0.0002870- 0.0002870) + 0.0002870);
+		enemies[i].lat += randomVelLat;
+		enemies[i].lng += randomVelLng;
+		
+		var newLocation=new google.maps.LatLng(
+			enemies[i].lat,
+			enemies[i].lng
+		);
+		enemies[i].markerE.setPosition(newLocation);
+		
+		
+	}
+	setTimeout(enemyAction,1000);
+	
+}
+
+
+function enemyBomb(){
+	for(var i=0; i<enemies.length; i++){
+		
+	dropBomb(10,enemies[i].markerE.getPosition(),enemy);
+	
+	}
+	
+	setTimeout(enemyBomb,10000);
+	
+}
+	
+	
+
+	
+
+
+	
---
+++ js/player.js
@@ -0,0 +1,50 @@
+var player = {id: 'player', life: 3 , numberBomb:1, playerRadius : 30, score: 0};
+
+function looseHealthPlayer(player){
+	player.life=player.life-1;
+	
+	if(player.life<=0){
+		window.location.replace("game_over.html")	
+	}
+	
+	if(player.id == 'player') {
+		document.getElementById("life").innerHTML = player.life;
+	}
+}
+
+function gainScore(player){
+	player.score=player.score+1;
+	
+	if(player.id == 'player') {
+		document.getElementById("score").innerHTML = player.score;
+	}
+}
+
+
+function dropBombP() {
+	if(player.numberBomb>0) {	
+		dropBomb(player.playerRadius,marker.getPosition(),player);
+		//PUTTING A BOMB ON THE GROUND REMOVES ONE OF YOUR BAG
+		player.numberBomb=player.numberBomb-1 ;
+		document.getElementById("numberBomb").innerHTML = player.numberBomb;
+	}
+}
+
+function addLife(){
+	player.life+=1;
+	document.getElementById("life").innerHTML = player.life;
+	
+}
+
+function addBomb(){
+	player.numberBomb+=1;
+	document.getElementById("numberBomb").innerHTML = player.numberBomb;
+}
+
+function addFire(){
+	player.playerRadius+=10;
+	document.getElementById("playerRadius").innerHTML = player.playerRadius;
+}
+
+
+	
